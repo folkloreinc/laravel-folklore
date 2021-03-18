08080255783b22ee1e8401358b4c3648f0435bef
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2021-03-18 09:35:13 -0400

    Removed facade

--- tests/FolkloreTestCase.php
+++ tests/FolkloreTestCase.php
@@ -3,25 +3,17 @@
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Orchestra\Testbench\TestCase;
 
-class FolkloreTestCase extends TestCase {
-
+class FolkloreTestCase extends TestCase
+{
     public function setUp()
     {
         parent::setUp();
 
-        $this->app->instance('path.public', __DIR__.'/fixture');
+        $this->app->instance('path.public', __DIR__ . '/fixture');
     }
 
     protected function getPackageProviders($app)
     {
-        return array('Folklore\ServiceProvider');
+        return ['Folklore\ServiceProvider'];
     }
-
-    protected function getPackageAliases($app)
-    {
-        return array(
-            'Folklore' => 'Folklore\Facade'
-        );
-    }
-
 }
