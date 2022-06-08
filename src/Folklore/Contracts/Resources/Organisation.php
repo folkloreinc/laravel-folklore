d9ef215095582cfc55c1dadb187d19f3c1a57d4a
Nicolas Roy B <phatshambler@gmail.com>
2022-06-08 15:08:36 -0400

    Contracts

--- src/Folklore/Contracts/Resources/Organisation.php
+++ src/Folklore/Contracts/Resources/Organisation.php
@@ -2,9 +2,13 @@
 
 namespace Folklore\Contracts\Resources;
 
+use Illuminate\Support\Collection;
+
 interface Organisation extends Resource
 {
     public function name(): string;
 
     public function slug(): string;
+
+    public function members(): ?Collection;
 }
