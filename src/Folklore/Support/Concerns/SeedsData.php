0be0a3f3e77b39028800bf579e87dcf13f7d9f39
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2022-11-28 10:17:36 -0500

    Added SeedsData traits "

---
+++ src/Folklore/Support/Concerns/SeedsData.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Folklore\Support\Concenrs;
+
+use Folklore\Support\Data;
+
+trait SeedsData
+{
+    public function loadJson($path)
+    {
+        return json_decode(file_get_contents($path), true);
+    }
+
+    public function replaceDataAtPaths($data, $paths, $replace)
+    {
+        return Data::reducePaths($paths, $data, function ($value, $path, $pathValue) use (
+            $replace
+        ) {
+            $newValue = $replace($pathValue, $path, $value);
+            data_set($value, $path, $newValue);
+            return $value;
+        });
+    }
+}
