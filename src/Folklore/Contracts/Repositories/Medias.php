69fdc3dfbe449c13d749563834c2e6d0b3b48347
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2023-01-06 10:25:41 -0500

    Updated medias repository

--- src/Folklore/Contracts/Repositories/Medias.php
+++ src/Folklore/Contracts/Repositories/Medias.php
@@ -2,6 +2,22 @@
 
 namespace Folklore\Contracts\Repositories;
 
+use Symfony\Component\HttpFoundation\File\File;
+use Folklore\Contracts\Resources\Media;
+
 interface Medias extends Resources
 {
+    public function findById(string $id): ?Media;
+
+    public function findByName(string $name): ?Media;
+
+    public function findByPath(string $path): ?Media;
+
+    public function create($data): Media;
+
+    public function update(string $id, $data): ?Media;
+
+    public function createFromFile(File $file, $data = []): Media;
+
+    public function createFromPath(string $path, $data = []): ?Media;
 }
