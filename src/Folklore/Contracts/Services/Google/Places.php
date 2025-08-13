00f2635fbd3a627cd445409c49e3c0b456a96c39
Nicolas Roy B. <phatshambler@gmail.com>
2025-08-13 13:13:52 -0400

    Google places

--- src/Folklore/Contracts/Services/Google/Places.php
+++ src/Folklore/Contracts/Services/Google/Places.php
@@ -4,9 +4,14 @@ namespace Folklore\Contracts\Services\Google;
 
 use Folklore\Contracts\Services\Google\Places\Region;
 use Folklore\Contracts\Services\Google\Places\Bounds;
+use Folklore\Contracts\Services\Google\Places\Location;
 
 interface Places
 {
+    public function findPlaceById(string $id): ?Location;
+
+    public function findPlaceByName(string $name): ?Location;
+
     public function findRegionByName(string $name): ?Region;
 
     public function findRegionBoundsByName(string $name): ?Bounds;
