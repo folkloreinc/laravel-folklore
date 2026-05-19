86028797d6a5398784458aa9fb84d5d3f003ae1e
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2026-05-19 15:55:21 -0400

    Fix with optional routes

--- src/Folklore/Composers/Concerns/ComposesRoutes.php
+++ src/Folklore/Composers/Concerns/ComposesRoutes.php
@@ -11,42 +11,51 @@ trait ComposesRoutes
     public function composeRoutesByNames($routesNames, $options = []): array
     {
         $routesCollection = resolve(Router::class)->getRoutes();
-        $routes = collect($routesNames)->map(function ($name) use ($routesCollection) {
-            return $routesCollection->getByName($name);
-        })->filter(function ($route) {
-            return !is_null($route);
-        })->values();
+        $routes = collect($routesNames)
+            ->map(function ($name) use ($routesCollection) {
+                return $routesCollection->getByName($name);
+            })
+            ->filter(function ($route) {
+                return !is_null($route);
+            })
+            ->values();
         return $this->composeRoutes($routes, $options);
     }
 
     public function composeRoutes($routes, $options = []): array
     {
-        $options = is_string($options) ? [
-            'namespaceToRemove' => $options
-        ] : $options;
+        $options = is_string($options)
+            ? [
+                'namespaceToRemove' => $options,
+            ]
+            : $options;
         $namespaceToRemove = Arr::get($options, 'namespaceToRemove');
-        return collect($routes)->mapWithKeys(function ($route) use ($namespaceToRemove, $options) {
-            $path = $this->getPathFromRoute($route, $options);
-            if (is_null($path)) {
-                return [];
-            }
-            $map = [];
-            $name = $route->getName();
-            $key = !is_null($namespaceToRemove)
-                ? preg_replace('/^'.preg_quote($namespaceToRemove, '/') . '\./', '$1', $name)
-                : $name;
-            $map[$key] = $path;
-            return $map;
-        })->toArray();
+        return collect($routes)
+            ->mapWithKeys(function ($route) use ($namespaceToRemove, $options) {
+                $path = $this->getPathFromRoute($route, $options);
+                if (is_null($path)) {
+                    return [];
+                }
+                $map = [];
+                $name = $route->getName();
+                $key = !is_null($namespaceToRemove)
+                    ? preg_replace('/^' . preg_quote($namespaceToRemove, '/') . '\./', '$1', $name)
+                    : $name;
+                $map[$key] = $path;
+                return $map;
+            })
+            ->toArray();
     }
 
     protected function getRoutesNamesWithLocales($names)
     {
         $locales = collect(config('locale.locales'));
         return collect($names)->reduce(function ($localizedRoutes, $routeName) use ($locales) {
-            return $localizedRoutes->merge($locales->map(function ($locale) use ($routeName) {
-                return $locale . '.' . $routeName;
-            }));
+            return $localizedRoutes->merge(
+                $locales->map(function ($locale) use ($routeName) {
+                    return $locale . '.' . $routeName;
+                })
+            );
         }, collect());
     }
 
@@ -69,29 +78,27 @@ trait ComposesRoutes
 
         $path = resolve(UrlGenerator::class)->route($name, $params, false);
 
-        if (!$withoutParametersPatterns) {
-            $optionalParameters = array_map(
-                function ($m) {
-                    return trim($m, '?');
-                },
-                array_values(
-                    array_filter($matches[1], function ($m) {
-                        return preg_match('/\?$/', $m) === 1;
-                    })
-                )
-            );
-            foreach ($parameters as $parameter) {
-                if (in_array($parameter, $optionalParameters)) {
-                    $path = preg_replace('/(' . preg_quote(':' . $parameter) . ')\b/i', '$1?', $path);
-                }
-                if (isset($patterns[$parameter])) {
-                    $pattern = preg_replace('/^\(?(.*?)\)?$/', '$1', $patterns[$parameter]);
-                    $path = preg_replace(
-                        '/(' . preg_quote(':' . $parameter) . ')(\?)?\b/i',
-                        '$1(' . $pattern . ')$2',
-                        $path
-                    );
-                }
+        $optionalParameters = array_map(
+            function ($m) {
+                return trim($m, '?');
+            },
+            array_values(
+                array_filter($matches[1], function ($m) {
+                    return preg_match('/\?$/', $m) === 1;
+                })
+            )
+        );
+        foreach ($parameters as $parameter) {
+            if (in_array($parameter, $optionalParameters)) {
+                $path = preg_replace('/(' . preg_quote(':' . $parameter) . ')\b/i', '$1?', $path);
+            }
+            if (isset($patterns[$parameter]) && !$withoutParametersPatterns) {
+                $pattern = preg_replace('/^\(?(.*?)\)?$/', '$1', $patterns[$parameter]);
+                $path = preg_replace(
+                    '/(' . preg_quote(':' . $parameter) . ')(\?)?\b/i',
+                    '$1(' . $pattern . ')$2',
+                    $path
+                );
             }
         }
 
